7a55ae1f5c7fd8826b734a64401d77a80dc05ebc
agrahariashish <81158424+agrahariashish@users.noreply.github.com>
2021-03-22 18:42:26 +0530

    Update Calculator.java

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -3,7 +3,7 @@ package main;
 public class Calculator {
 
 	public static void main(String[] args) {
-		System.out.println("hello sir");
+		System.out.println("hello");
 		add(10,20);
 	}
 		public static void add(int num1,int num2) {
