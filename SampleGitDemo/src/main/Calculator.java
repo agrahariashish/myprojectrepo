767118e1d883f7c5a212f0cef5bfe5ec86b7f9fb
agrahariashish <agrahariashish120@gmail.com>
2021-03-23 09:32:28 +0530

    added diff function

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -10,5 +10,13 @@ public class Calculator {
 		System.out.println(num1+num2);
 
 }
+		public static void sub(int num1,int num2) {
+		System.out.println(num1-num2);
+
+}
+		public static void mul(int num1,int num2) {
+		System.out.println(num1*num2);
+
+}
 
 }
